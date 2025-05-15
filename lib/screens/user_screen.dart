1fc4a91267e6f19ab78f9ed2abb89e7cac868dcd
shahz-codes <shahzali851@gmail.com>
2025-05-15 07:33:31 +0400

    Added a refresh button  in the appBar

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -5,34 +5,51 @@ import '../bloc/user_bloc.dart';
 import '../bloc/user_event.dart';
 import '../bloc/user_state.dart';
 import '../widgets/user_tile.dart';
-import '../models/user_model.dart';
 
 class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('User List')),
+      appBar: AppBar(
+        title: Text('User List'), // Title
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh), // Refresh icon
+            onPressed: () {
+              // When the refresh icon is tapped, trigger the LoadUsers event
+              context.read<UserBloc>().add(LoadUsers());
+            },
+          )
+        ],
+      ),
 
       body: BlocBuilder<UserBloc, UserState>(
         builder: (context, state) {
+          // loading spinner when app fetch's data
           if (state is UserLoading) {
-            // Show loading spinner while data being fetched
             return Center(child: CircularProgressIndicator());
-          } else if (state is UserLoaded) {
-            final users = state.users;
-            // Once loaded then shows a scrollable list of users
+          }
 
+          // data is successfully loaded, display in scrollable list
+          else if (state is UserLoaded) {
+            final users = state.users; // Extract list of users from the state
             return ListView.builder(
-              itemCount: users.length,
+              itemCount: users.length, // Total number of users
               itemBuilder: (context, index) {
+                // UserTile widget to display each user's data
                 return UserTile(user: users[index]);
               },
             );
-          } else if (state is UserError) {
-            // If an error occurs then it will show error_message
-            return Center(child: Text(state.message));
-          } else {
-            return Center(child: Text('Press the button to fetch users'));
+          }
+
+          // error block
+          else if (state is UserError) {
+            return Center(child: Text(state.message)); // Show error
+          }
+
+          // Default fallback
+          else {
+            return Center(child: Text('Press the refresh button to fetch users'));
           }
         },
       ),
